7a51be0b6a20d862c62f3b90fab6cfe0d54517b2
js <js@cos.pl>
2023-02-09 15:27:51 +0100

    09.02.2023

---
+++ src/components/AddUser.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AddUser() {
+    
+    return (
+        <form>
+            <label htmlFor="username">Username</label>
+            <input></input>
+        </form>
+    )
+}
+
+export default AddUser;
