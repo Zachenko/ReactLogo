2058b6a054296bedbbbae0781a501b488a09db7e
js <js@cos.pl>
2023-02-16 15:19:28 +0100

    16.02.2023

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 function AddUser() {
+
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
+    function namedChangeHandler(event) {
+        setName(event.target.value);
+        //console.dir(name)
+    }
+
+    function ageChangeHandler(event) {
+        setAge(event.target.value);
+        //  console.dir(age)
+    }
+
+    function addUserHandler(event) {
+        event.preventDefault();
+        console.dir(`${name} ${age}`)
+        setName('')
+        setAge('')
+    }
     
     return (
         <Card className={classes.input}>
-            <form>
+            <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text"/><br/><br/>
+                <input id="username" type="text"
+                    onChange={namedChangeHandler}
+                    value={name}
+                /><br/><br/>
 
                 <label htmlFor="age">Age</label>
-                <input id="age" type="number"/>
+                <input id="age" type="number"
+                    onChange={ageChangeHandler}
+                    value={age}
+                />
 
-                <button type="submit">Add user</button>
+                <Button myType="submit">Add user</Button>
             </form>
         </Card>
     )
