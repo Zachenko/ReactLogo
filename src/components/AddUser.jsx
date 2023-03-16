7e915d299b873734659b3f735aee31d5ae0bb149
js <js@cos.pl>
2023-03-16 15:19:47 +0100

    16.03.2023 (starting of homework)

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,27 +1,23 @@
-import React, { useState} from 'react'; 
+import React, { useState, useEffect, useCallback } from 'react'; 
 import Card from '../UI/Card';
 import classes from './AddUser.module.css' ;
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 
+// https://zbreactlogofb-default-rtdb.firebaseio.com/
+
 function AddUser() {
 
-    const [name, setName] = useState('')
-    const [age, setAge] = useState('')
-    const [errorModal, setErrorModal] = useState(null)
 
-    function namedChangeHandler(event) {
-        setName(event.target.value)
-    }
 
-    function ageChangeHandler(event) {
-        setAge(event.target.value);
-        console.log(age)
-    }
-
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorModal, setErrorModal] = useState(null);
+    
     function addUserHandler(event) {
         event.preventDefault();
-        //console.log(name, age);
 
         if (+age < 1) {
             setErrorModal({
@@ -32,6 +28,34 @@ function AddUser() {
 
         setAge('');
         setName('');
+    }
+
+    const getDataHandler = useCallback(async () => {
+        const responce = await fetch('https://zbreactlogofb-default-rtdb.firebaseio.com/react_logowanie.json');
+        const fbData = await responce.json();
+        const loadedData = [];
+
+        // for (const key in fbData) {
+        //     loadedData.push({
+
+        //     })
+        // }
+    })
+
+    function namedChangeHandler(event) {
+        setName(event.target.value)
+    }
+
+    function ageChangeHandler(event) {
+        setAge(event.target.value);
+    }
+
+    function emailChangeHandler(event) {
+        setEmail(event.target.value);
+    }
+
+    function passwordChangeHandler(event) {
+        setPassword(event.target.value);
     }
 
     const errorHandler = () => {
@@ -50,12 +74,25 @@ function AddUser() {
                 />
 
                 <label htmlFor="age">Age</label>
-                <input id="age" type="Number" 
-                        onChange={ageChangeHandler}
-                        value={age}
+                <input id="age" type="number" 
+                    onChange={ageChangeHandler}
+                    value={age}
+                />
+
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email" 
+                    onChange={emailChangeHandler}
+                    value={email}
+                />
+
+                <label htmlFor="password">Password</label>
+                <input id="password" type="password" 
+                    onChange={passwordChangeHandler}
+                    value={password}
                 />
 
                 <Button myType="submit"> Add user </Button>
+                <Button myType="button" onClick={getDataHandler}> Get data </Button>
             </form>
             </Card>
         </>
